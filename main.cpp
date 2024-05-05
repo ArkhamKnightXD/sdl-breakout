df39865deb10766f1609329a500c9fca3365b168
knight <karvin_jimenez@hotmail.com>
2024-05-04 22:41:27 -0400

    breakout finish.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,60 @@
 #include <iostream>
+#include <vector>
 #include "inc/SDL.h"
 
-SDL_Window* window = nullptr;
-SDL_Renderer* renderer = nullptr;
-SDL_Rect rectangle;
+SDL_Window *window = nullptr;
+SDL_Renderer *renderer = nullptr;
+SDL_Rect player;
+SDL_Rect ball;
 
-const int SPEED = 600;
+int playerSpeed = 800;
+int ballVelocityX = 400;
+int ballVelocityY = 400;
+
 const int SCREEN_WIDTH = 960;
-const int SCREEN_HEIGHT = 544;
-const int FRAME_RATE = 60; // Desired frame rate (frames per second)
+const int SCREEN_HEIGHT = 640;
 
-// Function to handle events
-void handleEvents() {
+typedef struct
+{
+    SDL_Rect bounds;
+    bool isDestroyed;
+} Brick;
 
+std::vector<Brick> bricks;
+
+std::vector<Brick> createBricks()
+{
+    std::vector<Brick> bricks;
+
+    int positionX;
+    int positionY = 40;
+
+    for (int i = 0; i < 8; i++)
+    {
+        positionX = 0;
+
+        for (int j = 0; j < 15; j++)
+        {
+            Brick actualBrick = {{positionX, positionY, 60, 20}, false};
+
+            bricks.push_back(actualBrick);
+            positionX += 64;
+        }
+
+        positionY += 22;
+    }
+
+    return bricks;
+}
+
+void handleEvents()
+{
     SDL_Event event;
 
-    while (SDL_PollEvent(&event)) {
-
-        if (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE) {
-            
+    while (SDL_PollEvent(&event))
+    {
+        if (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE)
+        {
             SDL_DestroyRenderer(renderer);
             SDL_DestroyWindow(window);
             SDL_Quit();
@@ -27,100 +63,123 @@ void handleEvents() {
     }
 }
 
-// Function to update rectangle movement.
-void update(float deltaTime) {
-
-    const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
-
-    if (currentKeyStates[SDL_SCANCODE_W] && rectangle.y > 0) {
-        rectangle.y -= SPEED * deltaTime;
-    }
-
-    else if (currentKeyStates[SDL_SCANCODE_S] && rectangle.y < SCREEN_HEIGHT - rectangle.h) {
-        rectangle.y += SPEED * deltaTime;
-    }
-
-    else if (currentKeyStates[SDL_SCANCODE_A] && rectangle.x > 0) {
-        rectangle.x -= SPEED * deltaTime;
-    }
-
-    else if (currentKeyStates[SDL_SCANCODE_D] && rectangle.x < SCREEN_WIDTH - rectangle.w) {
-        rectangle.x += SPEED * deltaTime;
-    }
+bool hasCollision(SDL_Rect bounds, SDL_Rect ball)
+{
+    return bounds.x < ball.x + ball.w && bounds.x + bounds.w > ball.x &&
+           bounds.y < ball.y + ball.h && bounds.y + bounds.h > ball.y;
 }
 
-// Function to render graphics
-void render() {
-    // Clear the renderer
+void update(float deltaTime)
+{
+    const Uint8 *currentKeyStates = SDL_GetKeyboardState(NULL);
+
+    if (player.x > 0 && currentKeyStates[SDL_SCANCODE_A])
+    {
+        player.x -= playerSpeed * deltaTime;
+    }
+
+    else if (player.x < SCREEN_WIDTH - 74 && currentKeyStates[SDL_SCANCODE_D])
+    {
+        player.x += playerSpeed * deltaTime;
+    }
+
+    if (ball.y > SCREEN_HEIGHT + 32 || ball.y < -32)
+    {
+        ball.x = SCREEN_WIDTH / 2 - 32;
+        ball.y = SCREEN_HEIGHT / 2 - 32;
+    }
+
+    if (ball.x < 0 || ball.x > SCREEN_WIDTH - 32)
+    {
+        ballVelocityX *= -1;
+    }
+
+    if (hasCollision(player, ball))
+    {
+        ballVelocityY *= -1;
+    }
+
+    for (unsigned int i = 0; i < bricks.size(); i++)
+    {
+        if (!bricks[i].isDestroyed && hasCollision(bricks[i].bounds, ball))
+        {
+            ballVelocityY *= -1;
+
+            bricks[i].isDestroyed = true;
+        }
+    }
+
+    ball.x += ballVelocityX * deltaTime;
+    ball.y += ballVelocityY * deltaTime;
+}
+
+void render()
+{
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-    // Set drawing color to white
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-    // Render the rectangle
-    SDL_RenderFillRect(renderer, &rectangle);
-
-    // Present the renderer
-    SDL_RenderPresent(renderer);
-}
-
-// Function to cap frame rate
-void capFrameRate(Uint32 frameStartTime) {
-
-    Uint32 frameTime = SDL_GetTicks() - frameStartTime;
-    
-    if (frameTime < 1000 / FRAME_RATE) {
-        SDL_Delay(1000 / FRAME_RATE - frameTime);
+    for (Brick brick : bricks)
+    {
+        if (!brick.isDestroyed)
+            SDL_RenderFillRect(renderer, &brick.bounds);
     }
+
+    SDL_RenderFillRect(renderer, &player);
+    SDL_RenderFillRect(renderer, &ball);
+
+    SDL_RenderPresent(renderer);
 }
 
 #undef main
 
-int main() {
-
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+int main()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
         std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
         return 1;
     }
 
     window = SDL_CreateWindow("My Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (window == nullptr) {
+    if (window == nullptr)
+    {
         std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
     }
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (renderer == nullptr) {
+    if (renderer == nullptr)
+    {
         std::cerr << "Failed to create renderer: " << SDL_GetError() << std::endl;
         SDL_DestroyWindow(window);
         SDL_Quit();
         return 1;
     }
 
-    rectangle = {SCREEN_WIDTH / 2 - 16, SCREEN_HEIGHT / 2 - 16, 32, 32};
+    bricks = createBricks();
+
+    player = {SCREEN_WIDTH / 2, SCREEN_HEIGHT - 32, 74, 16};
+
+    ball = {SCREEN_WIDTH / 2 - 20, SCREEN_HEIGHT / 2 - 20, 20, 20};
 
     Uint32 previousFrameTime = SDL_GetTicks();
     Uint32 currentFrameTime = previousFrameTime;
     float deltaTime = 0.0f;
 
-    // Main loop
-    while (true) {
-        
+    while (true)
+    {
         currentFrameTime = SDL_GetTicks();
 
-    // Calculate delta time in seconds
         deltaTime = (currentFrameTime - previousFrameTime) / 1000.0f; // Convert to seconds
 
-    // Update the previous frame time for the next iteration
         previousFrameTime = currentFrameTime;
 
         handleEvents();
         update(deltaTime);
         render();
-
-        // capFrameRate(currentFrameTime);
     }
 
     SDL_DestroyRenderer(renderer);
