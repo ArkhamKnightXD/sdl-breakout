aa0d02bf51e2ed2a0b36e1f7af7a3388823a7d23
knight <karvin_jimenez@hotmail.com>
2024-06-02 13:50:50 -0400

    extra changes.

--- main.cpp
+++ main.cpp
@@ -17,25 +17,25 @@ int ballVelocityY = 425;
 
 bool isAutoPlayMode = false;
 
-typedef struct
-{
+typedef struct {
+    
     SDL_Rect bounds;
     bool isDestroyed;
 } Brick;
 
-std::vector<Brick> createBricks()
-{
+std::vector<Brick> createBricks() {
+
     std::vector<Brick> bricks;
 
     int positionX;
     int positionY = 40;
 
-    for (int row = 0; row < 8; row++)
-    {
+    for (int row = 0; row < 8; row++) {
+
         positionX = 0;
 
-        for (int column = 0; column < 15; column++)
-        {
+        for (int column = 0; column < 15; column++) {
+
             Brick actualBrick = {{positionX, positionY, 60, 20}, false};
 
             bricks.push_back(actualBrick);
@@ -61,18 +61,18 @@ void handleEvents()
 {
     SDL_Event event;
 
-    while (SDL_PollEvent(&event))
-    {
-        if (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE)
-        {
+    while (SDL_PollEvent(&event)) {
+
+        if (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE) {
+
             quitGame();
             exit(0);
         }
     }
 }
 
-bool hasCollision(SDL_Rect bounds, SDL_Rect ball)
-{
+bool hasCollision(SDL_Rect bounds, SDL_Rect ball) {
+
     return bounds.x < ball.x + ball.w && bounds.x + bounds.w > ball.x &&
            bounds.y < ball.y + ball.h && bounds.y + bounds.h > ball.y;
 }
@@ -81,53 +81,46 @@ void update(float deltaTime)
 {
     const Uint8 *currentKeyStates = SDL_GetKeyboardState(NULL);
 
-    if (currentKeyStates[SDL_SCANCODE_W])
-    {
+    if (currentKeyStates[SDL_SCANCODE_W]) {
         isAutoPlayMode = !isAutoPlayMode;
     }
 
-    if (isAutoPlayMode && ball.x < SCREEN_WIDTH - player.w)
-    {
+    if (isAutoPlayMode && ball.x < SCREEN_WIDTH - player.w) {
         player.x = ball.x;
     }
 
-    if (player.x > 0 && currentKeyStates[SDL_SCANCODE_A])
-    {
+    if (player.x > 0 && currentKeyStates[SDL_SCANCODE_A]) {
         player.x -= playerSpeed * deltaTime;
     }
 
-    else if (player.x < SCREEN_WIDTH - player.w && currentKeyStates[SDL_SCANCODE_D])
-    {
+    else if (player.x < SCREEN_WIDTH - player.w && currentKeyStates[SDL_SCANCODE_D]) {
         player.x += playerSpeed * deltaTime;
     }
 
-    if (ball.y > SCREEN_HEIGHT + ball.h)
-    {
+    if (ball.y > SCREEN_HEIGHT + ball.h) {
+
         ball.x = SCREEN_WIDTH / 2 - ball.w;
         ball.y = SCREEN_HEIGHT / 2 - ball.h;
 
         ballVelocityX *= -1;
     }
 
-    if (ball.y < 0)
-    {
+    if (ball.y < 0) {
         ballVelocityY *= -1;
     }
     
-    if (ball.x < 0 || ball.x > SCREEN_WIDTH - ball.w)
-    {
+    if (ball.x < 0 || ball.x > SCREEN_WIDTH - ball.w) {
         ballVelocityX *= -1;
     }
 
-    if (hasCollision(player, ball))
-    {
+    if (hasCollision(player, ball)) {
         ballVelocityY *= -1;
     }
 
-    for (unsigned int i = 0; i < bricks.size(); i++)
-    {
-        if (!bricks[i].isDestroyed && hasCollision(bricks[i].bounds, ball))
-        {
+    for (unsigned int i = 0; i < bricks.size(); i++) {
+
+        if (!bricks[i].isDestroyed && hasCollision(bricks[i].bounds, ball)) {
+
             ballVelocityY *= -1;
             bricks[i].isDestroyed = true;
         }
@@ -144,8 +137,8 @@ void render()
 
     SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
 
-    for (Brick brick : bricks)
-    {
+    for (Brick brick : bricks) {
+
         if (!brick.isDestroyed)
             SDL_RenderFillRect(renderer, &brick.bounds);
     }
@@ -162,23 +155,20 @@ void render()
 
 int main()
 {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
         return 1;
     }
 
     window = SDL_CreateWindow("My Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (window == nullptr)
-    {
+    if (window == nullptr) {
         std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
     }
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (renderer == nullptr)
-    {
+    if (renderer == nullptr) {
         std::cerr << "Failed to create renderer: " << SDL_GetError() << std::endl;
         SDL_DestroyWindow(window);
         SDL_Quit();
@@ -189,8 +179,8 @@ int main()
     Uint32 currentFrameTime = previousFrameTime;
     float deltaTime = 0.0f;
 
-    while (true)
-    {
+    while (true) {
+
         currentFrameTime = SDL_GetTicks();
         deltaTime = (currentFrameTime - previousFrameTime) / 1000.0f;
         previousFrameTime = currentFrameTime;
